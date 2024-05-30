109f6315335c6f06a88e0bbc14dcfc879206dddb
Ivaneminem <ivanbondarenko@knu.ua>
2024-05-30 20:06:45 +0300

    Add ball_check_collision

--- ball.py
+++ ball.py
@@ -16,3 +16,21 @@ class Ball:
 
     def draw(self, surface):
         pygame.draw.circle(surface, pygame.Color('white'), self.rect.center, BALL_RADIUS)
+
+    def check_collision(self, paddle, blocks, colors):
+        if self.rect.centerx < BALL_RADIUS or self.rect.centerx > WIDTH - BALL_RADIUS:
+            self.dx = -self.dx
+        if self.rect.centery < BALL_RADIUS:
+            self.dy = -self.dy
+
+        if self.rect.colliderect(paddle.rect) and self.dy > 0:
+            self.dx, self.dy = detect_collision(self.dx, self.dy, self.rect, paddle.rect)
+
+        hit_index = self.rect.collidelist(blocks)
+        if hit_index != -1:
+            hit_rect = blocks.pop(hit_index)
+            hit_color = colors.pop(hit_index)
+            self.dx, self.dy = detect_collision(self.dx, self.dy, self.rect, hit_rect)
+            hit_rect.inflate_ip(self.rect.width * 3, self.rect.height * 3)
+            return hit_rect, hit_color
+        return None, None

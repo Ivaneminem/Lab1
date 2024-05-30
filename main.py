8a5a1ec03a90702c13040772068681ca0b7d7d98
Ivaneminem <ivanbondarenko@knu.ua>
2024-05-30 19:22:19 +0300

    add init_pygame

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ WIDTH, HEIGHT = 1200, 800
 IMG_PATH = '1.jpg'
 SCORE_FILE = 'scores.txt'
 
+# Initialize pygame
+pygame.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+img = pygame.image.load(IMG_PATH).convert()
+

d987715c005ddc0076963bb014c9e526aaea244e
Ivaneminem <ivanbondarenko@knu.ua>
2024-05-30 19:31:27 +0300

    add draw_text

--- helpers.py
+++ helpers.py
@@ -19,3 +19,10 @@ def detect_collision(dx, dy, ball, rect):
     elif delta_y > delta_x:
         dx = -dx
     return dx, dy
+
+def draw_text(surface, text, size, x, y):
+    font = pygame.font.Font(pygame.font.match_font('arial'), size)
+    text_surface = font.render(text, True, pygame.Color('white'))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surface.blit(text_surface, text_rect)

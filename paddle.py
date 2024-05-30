1dbd5d99393083271e028db800e56b8926fafd38
Ivaneminem <ivanbondarenko@knu.ua>
2024-05-30 19:59:03 +0300

    Add paddle_move

--- paddle.py
+++ paddle.py
@@ -4,3 +4,9 @@ from config import WIDTH, HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_SPEED
 class Paddle:
     def __init__(self):
         self.rect = pygame.Rect(WIDTH // 2 - PADDLE_WIDTH // 2, HEIGHT - PADDLE_HEIGHT - 10, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    def move(self, left, right):
+        if left and self.rect.left > 0:
+            self.rect.left -= PADDLE_SPEED
+        if right and self.rect.right < WIDTH:
+            self.rect.right += PADDLE_SPEED
